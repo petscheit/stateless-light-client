c603eca8b76f9b102df89bf57cc2cbf8e8af8e32
petscheit <etscheitp@gmail.com>
2025-06-29 02:36:27 +0200

    feat: added settlement example

--- src/crates/shikai/src/main.rs
+++ src/crates/shikai/src/main.rs
@@ -1,39 +1,75 @@
-use alloy_primitives::{hex::FromHex, FixedBytes};
+use alloy_primitives::{hex::{self, FromHex}, keccak256, Address, FixedBytes, TxKind, U256};
 use dotenv::from_filename;
 use shikai::Shikai;
+
+pub struct ClaimedExecution {
+    pub tx_hash: FixedBytes<32>,
+    pub receiver: Address,
+    pub amount: U256,
+}
+
+impl ClaimedExecution {
+    pub async fn verify(&self, shikai: &Shikai) -> bool {
+        let tx = shikai.execution().tx(self.tx_hash).await.unwrap();
+
+        if tx.value() != self.amount {
+            return false;
+        }
+
+        if tx.to() != TxKind::Call(self.receiver) {
+            return false;
+        }
+
+        true
+    }
+
+    pub fn id(&self) -> FixedBytes<32> {
+        let mut data = Vec::new();
+        data.extend_from_slice(&self.receiver.as_slice());
+        data.extend_from_slice(&self.amount.as_le_slice());
+        FixedBytes::from(keccak256(data))
+    }
+}
+
+
+
 
 #[tokio::main]
 async fn main() {
     from_filename(".env.local").ok();
-
-    // let block_number = 8457416;
-
-    // let address = Address::from_hex("0xE919522e686D4e998e0434488273C7FA2ce153D8").unwrap();
-    // let slot_number = 7815231;
 
     let shikai = Shikai::new(
         std::env::var("EXECUTION_RPC_URL").unwrap(),
         std::env::var("BEACON_RPC_URL").unwrap(),
     );
 
-    let tx_hash = FixedBytes::<32>::from_hex(
-        "0x47cea127fc2dcf17430191190d7edfb4ce971d82e8bef7a8ec866b66512e53c5",
-    )
-    .unwrap();
+    let claimed_executions = get_claimed_executions();
+    let mut valid_executions = Vec::new();
 
-    let tx = shikai.execution().tx(tx_hash).await.unwrap();
-    println!("Verified Transaction: {:?}", tx.0);
+    for claimed_execution in claimed_executions {
+        let verified = claimed_execution.verify(&shikai).await;
+        if verified {
+            valid_executions.push(claimed_execution.id());
+        }
+    }
 
-    // let account = shikai
-    //     .execution()
-    //     .account(address, block_number)
-    //     .await
-    //     .unwrap();
-    // println!("Verified Account: {:?}", account);
+    println!("Valid Executions: {:?}", valid_executions);
 
-    // let execution_header = shikai.execution().header(block_number).await.unwrap();
-    // println!("Verified Execution Header: {:?}", execution_header.0);
+} 
 
-    // let beacon_header = shikai.beacon().header(slot_number).await.unwrap();
-    // println!("Verified Beacon Header: {:?}", beacon_header.0);
+fn get_claimed_executions() -> Vec<ClaimedExecution> {
+    let claimed_executions = vec![
+        ClaimedExecution {
+            tx_hash: FixedBytes::<32>::from_hex("0x47cea127fc2dcf17430191190d7edfb4ce971d82e8bef7a8ec866b66512e53c5").unwrap(),
+            receiver: Address::from_hex("0x6c5aAE4622B835058A41879bA5e128019B9047d6").unwrap(),
+            amount: U256::from_be_slice(hex::decode("0x012A02FE3A6448D800").unwrap().as_slice()),
+        },
+        ClaimedExecution {
+            tx_hash: FixedBytes::<32>::from_hex("0x76f99c726031d88bc4a78e39fba1304d458587b8d018fe6660e4d6ff8b6e337a").unwrap(),
+            receiver: Address::from_hex("0x327FDab86397F41Ed67df6419A5260865BE0523B").unwrap(),
+            amount: U256::from_be_slice(hex::decode("0x038D7EA4C68000").unwrap().as_slice()),
+        },
+    ];
+
+    claimed_executions
 }
